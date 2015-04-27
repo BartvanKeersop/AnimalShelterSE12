262382f19819da237bdc952ee6540f066bf26488
Bart van Keersop <b.vankeersop@student.fontys.nl>
2015-04-27 20:28:05 +0200

    Commit
    commit

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.tbPath = New System.Windows.Forms.TextBox()
+        Me.btnChooseFile = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(12, 22)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(623, 406)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'tbPath
+        '
+        Me.tbPath.Location = New System.Drawing.Point(12, 434)
+        Me.tbPath.Name = "tbPath"
+        Me.tbPath.Size = New System.Drawing.Size(513, 22)
+        Me.tbPath.TabIndex = 2
+        '
+        'btnChooseFile
+        '
+        Me.btnChooseFile.Location = New System.Drawing.Point(12, 462)
+        Me.btnChooseFile.Name = "btnChooseFile"
+        Me.btnChooseFile.Size = New System.Drawing.Size(132, 32)
+        Me.btnChooseFile.TabIndex = 3
+        Me.btnChooseFile.Text = "ChooseFile"
+        Me.btnChooseFile.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(150, 462)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(132, 32)
+        Me.btnStop.TabIndex = 4
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(288, 462)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(132, 32)
+        Me.btnStart.TabIndex = 5
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(854, 555)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnChooseFile)
+        Me.Controls.Add(Me.tbPath)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents tbPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnChooseFile As System.Windows.Forms.Button
+    Friend WithEvents btnStop As System.Windows.Forms.Button
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+
+End Class
